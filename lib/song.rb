b913eba58d5bb374351105275a0d5fe0763d4105
Matt Farncombe <matt@farncombe.co>
2020-05-22 01:50:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,12 +36,17 @@ class Song
   end
   
   def self.find_or_create_by_name(name)
+<<<<<<< HEAD
     song_found = find_song = find_by_name(name)
     if song_found == nil 
       create_song = create_by_name(name)
     else
       find_song = find_by_name(name)
     end
+=======
+    find_song = find_by_name(name)
+    create_song = create_by_name(name)
+>>>>>>> 2b9d3a9d2945714996a4b5eb4ff92b0715fe2bc9
   end
   
   def self.alphabetical
@@ -50,6 +55,7 @@ class Song
     self.all.each do |data|
       sorted_songs << data
     end
+<<<<<<< HEAD
     sorted_songs.sort! { |a, b|  a.name <=> b.name }
   end
   
@@ -59,14 +65,23 @@ class Song
     song.name = filename_array[1][1..-5]
     song.artist_name = filename_array[0][0..-2]
     song
+=======
+    sorted_songs
+>>>>>>> 2b9d3a9d2945714996a4b5eb4ff92b0715fe2bc9
   end
   
   def self.create_from_filename(filename)
     filename_array = filename.split("-")
     song = self.new
+<<<<<<< HEAD
     song.name = filename_array[1][1..-5]
     song.artist_name = filename_array[0][0..-2]
     @@all << song
+=======
+    song.name = filename_array[1].slice(-1)
+    song.artist_name = filename_array[0]
+    binding.pry
+>>>>>>> 2b9d3a9d2945714996a4b5eb4ff92b0715fe2bc9
     song
   end
   
